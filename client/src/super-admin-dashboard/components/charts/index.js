16463ad6da424d247860a436620fbbbf7193f61b
Radharaman Gurjar <156350993+Radharamangurjar315@users.noreply.github.com>
2025-08-24 08:13:05 +0530

    Delete client directory

--- client/src/super-admin-dashboard/components/charts/index.js
+++
@@ -1,6 +0,0 @@
-export {
-  StatusBarChart,
-  PriorityDoughnutChart,
-  DepartmentPieChart,
-} from "./OverviewCharts";
-export { TrendsLineChart, SLAAreaChart } from "./TrendChart";
--- client/src/super-admin-dashboard/components/ui/index.js
+++
@@ -1,4 +0,0 @@
-export { default as StatsCard } from "./StatsCard";
-export { default as LoadingSpinner } from "./LoadingSpinner";
-export { default as DataTable } from "./DataTable";
-export { default as FilterPanel } from "./FilterPanel";
